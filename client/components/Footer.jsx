80d6e16fd8bd33632c92c8f0cb01678299a57e56
Vilas-cp <vilaspgowda1000@gmail.com>
2024-02-14 13:52:46 +0530

    footer

--- client/components/Footer.jsx
+++ client/components/Footer.jsx
@@ -7,14 +7,14 @@ const Footer = () => {
         <div className='p-[70px] flex justify-between 
             bg-[#1f1f1f] '>
             <div className='flex flex-col gap-6'>
-            <h1 className='font-extrabold text-2xl'>Machalang.lang</h1>
+            <h1 className='font-extrabold text-2xl '>Machalang.lang</h1>
             <div className='flex justify-between items-center  '>
             <img src='	https://cdn-icons-png.flaticon.com/128/5968/5968830.png' width={30} />
             <img src=' 	https://cdn-icons-png.flaticon.com/128/1384/1384014.png' width={35} />
             <img src='	https://cdn-icons-png.flaticon.com/128/1384/1384031.png' width={32} />
             <img src='	https://cdn-icons-png.flaticon.com/128/5968/5968968.png' width={33} />
             </div>
-            <h1 className='font-extrabold text-2xl text-yellow-200 hover:underline'>Give Feedback</h1>
+            <h1 className='font-extrabold text-2xl text-yellow-200 hover:underline font-sans'>Give Feedback</h1>
             <h1 className='text-xs text-gray-400'>© MachaLang 2024</h1>
             </div>
             <div className='flex flex-col gap-4'>
