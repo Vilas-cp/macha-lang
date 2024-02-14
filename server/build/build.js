5a7dcd84c8795d07487e1cc511c356f51258040a
ManojaD2004 <manojadkc2004@gmail.com>
2024-02-15 02:31:20 +0530

    Removed Some gitignore, WIll think about it in the future

---
+++ server/build/build.js
@@ -0,0 +1,4 @@
+let a = 100;
+let b = 20;
+for (let i = 0; i<=10; i++) { console.log((a+b)+(8+9));
+};
---
+++ server/build/bundle.js
@@ -0,0 +1,1 @@
+!function(e){"function"==typeof define&&define.amd?define(e):e()}((function(){"use strict";for(let e=0;e<=10;e++)console.log(30)}));
