770a05d45edf80ee4f0cf3a3042b66a91ddf2260
ManojaD2004 <manojadkc2004@gmail.com>
2024-02-15 20:29:48 +0530

    Added .gitignore

--- server/build/build.js
+++
@@ -1,5 +0,0 @@
-console.log('Namaste Macha');
-let name = "BGSCET";
-for (let i=0;i<10;i++) {
-    console.log("Hello "  + name);
-};
--- server/build/bundle.js
+++
@@ -1,1 +0,0 @@
-!function(e){"function"==typeof define&&define.amd?define(e):e()}((function(){"use strict";console.log("Namaste Macha");for(let e=0;e<10;e++)console.log("Hello BGSCET")}));
