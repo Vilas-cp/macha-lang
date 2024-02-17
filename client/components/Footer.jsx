c5133e2140f5b003f31cae334b081e8db91c874a
srinivasan-ad <forprogramingpurpose@gmail.com>
2024-02-17 22:34:13 +0530

    added little mobile opt

--- client/components/Footer.jsx
+++ client/components/Footer.jsx
@@ -4,7 +4,7 @@ const Footer = () => {
    
   return (
     <footer className='text-white '>
-        <div className='p-[70px] flex justify-between 
+        <div className='p-[70px] flex flex-col lg:flex-row lg:justify-between justify-center items-center text-center
             bg-[#1f1f1f] '>
             <div className='flex flex-col gap-6'>
             <h1 className='font-extrabold text-2xl '>Machalang.lang</h1>
@@ -15,10 +15,16 @@ const Footer = () => {
             <img src='	https://cdn-icons-png.flaticon.com/128/5968/5968968.png' width={33} />
             </div>
             <h1 className='font-extrabold text-2xl text-yellow-200 hover:underline font-sans'>Give Feedback</h1>
+           <div className='flex justify-center items-center text-center lg:hidden'><p className='font-light '>BGS College of Engineering And Technology 
+                    <br></br>
+                Mahalakshmi Layout 
+                <br></br>
+                Banglore
+                <br></br>
+                +91 9876543210</p></div>
             <h1 className='text-xs text-gray-400'>© MachaLang 2024</h1>
             </div>
-            <div className='flex flex-col gap-4'>
-                <h1 className='text-xl font-bold'>Contact Us</h1>
+            <div className='hidden  lg:flex flex-col gap-4'>
                 <p className='font-light '>BGS College of Engineering And Technology 
                     <br></br>
                 Mahalakshmi Layout 
@@ -26,6 +32,7 @@ const Footer = () => {
                 Banglore
                 <br></br>
                 +91 9876543210
+                <h1 className='text-xl font-bold'>Contact Us</h1>
                 <br></br>
                 +91 1234567890
                 </p>
--- client/components/Hero.jsx
+++ client/components/Hero.jsx
@@ -6,7 +6,7 @@ import { TypeAnimation } from 'react-type-animation';
 const Hero = () => {
   let router=useRouter();
     return (
-    <div className='text-black pt-[60px]'>
+    <div className='text-black pt-[60px] '>
         <div className='w-full h-[500px] bg-[#282A35!important] 
         flex items-center justify-center flex-col gap-[30px]'>
             <div className='font-extrabold text-[75px] text-white'>
@@ -15,27 +15,29 @@ const Hero = () => {
         
         'ಮಚ್ಚ ಲ್ಯಾಂಗ್',
         1000,
-        'Macha Lang',
+
+        'Macha  Lang',
         1000
       ]}
       wrapper="span"
       speed={50}
-      style={{ fontSize: '1em', display: 'inline-block' }}
+      style={{ fontSize: '.6em', display: 'inline-block' }}
       repeat={Infinity}
     />
             </div>
-            <div className='text-[#FFF4A3!important] font-bold text-[25px]'>ಕರ್ನಾಟಕದಲ್ಲಿ ಕನ್ನಡಿಗನೇ ಸಾರ್ವಭೌಮ</div>
+            <div className='text-[#FFF4A3!important] font-bold text-[25px] hidden lg:block'>ಕರ್ನಾಟಕದಲ್ಲಿ  ಕನ್ನಡಿಗನೇ ಸಾರ್ವಭೌಮ</div>
+            <div className='text-[#FFF4A3!important] font-bold text-[25px] lg:hidden text-center'>   ಕರ್ನಾಟಕದಲ್ಲಿ  <br></br>ಕನ್ನಡಿಗನೇ ಸಾರ್ವಭೌಮ</div>
             <button className='bg-[#04AA6D] h-[35px] rounded-3xl w-[250px] text-white font-semibold hover:bg-[#028555]' onClick={()=>router.push("Docs")}>ಕಲಿಯಿರಿ  -&gt;</button>
             
             </div>
-    <div className='w-full h-[500px] bg-[#D9EEE1!important]'>
-        <div className='flex justify-center items-center pt-[90px] gap-[40px]'>
+    <div className='lg:w-full  h-[500px] bg-[#D9EEE1!important]'>
+        <div className='flex flex-col lg:flex-row justify-center items-center pt-[90px] lg:gap-[40px] gap-[40px] '>
             <div>
-                <div className='text-[60px] font-bold'>About lang</div>
+                <div className='lg:text-[60px] text-[40px] font-bold pb-4'>About lang</div>
         <div className='text-xl font-semibold'>The language for kannadigas</div>
         <div>"code in your regional language"</div>
         </div>
-    <div className="flex justify-center w-[400px]">
+    <div className="flex justify-center lg:w-[400px] w-64 mr-4 lg:mr-1">
       <div className="bg-[#E7E9EB] flex flex-col pl-6 gap-[15px] pb-3 w-[100vh] pr-6 shadow-2xl ">
         <div className="font-semibold text-[30px] pt-2">Example</div>
         <div className="bg-white font-mono font-semi ">
