fd5bdc00e9da4a4fe3937867c78f276ddb7c4710
ManojaD2004 <manojadkc2004@gmail.com>
2024-02-19 01:34:03 +0530

    Title of Pages Added

--- client/app/Aboutus/layout.js
+++ client/app/Aboutus/layout.js
@@ -4,7 +4,7 @@ import { Inter } from "next/font/google";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
-  title: "Macha Lang",
+  title: "MachaLang AboutUs",
   description: "Kannada's its own coding language",
 };
 
